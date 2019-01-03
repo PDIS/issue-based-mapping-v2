5c4dea4032c520d50244946808119613cbd04431
XanxusX <tako30215@hotmail.com>
2019-01-03 09:43:01 +0800

    Fix bugs

--- src/components/board.vue
+++ src/components/board.vue
@@ -671,7 +671,7 @@ export default {
               that.success = '新增成功'
               that.snackbar = true
               that.dialog = false
-              that.getcards()
+              that.getlists()
             }
           })
         }
@@ -691,7 +691,7 @@ export default {
               that.success = '修改成功'
               that.snackbar = true
               that.dialog = false
-              that.getcards()
+              that.getlists()
             }
           })
         }
